3d92bc33fb60ca9cbc28fea0428860d239d63793
Mabry Cervin <mpcervin@uncg.edu>
2014-05-21 17:57:14 -0400

    Initial implementation of UQuantity in core.py

--- uquantity/core.py
+++ uquantity/core.py
@@ -1,7 +1,13 @@
-
+from astropy import units as u
+from uncertainties import ufloat
 
 
 class UQuantity(object):
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, value, unit, uncertainty):
+        self.value = value
+        self.unit = unit
+        self.uncertainty = uncertainty
+
+        self.quantity = value * unit
+        self.uncertObject = ufloat(value, uncertainty)
