9fae9f585e4445cb143e602a214e6b1b72b0e0c3
Mabry Cervin <mpcervin@uncg.edu>
2014-05-21 18:12:53 -0400

    Added tests for addition and subtraction

--- uquantity/tests/test_quncertainty.py
+++ uquantity/tests/test_quncertainty.py
@@ -1,5 +1,6 @@
 from astropy import units as u
 from uquantity import UQuantity
+import math
 
 
 def test_simple_uquantity():
@@ -10,3 +11,24 @@ def test_simple_uquantity():
     assert a.unit == u.km
     assert a.uncertainty == 2
 
+def test_addition_uquantity():
+    ###
+    a = UQuantity(5, u.km, 2)
+    b = UQuantity(12, u.km, 5)
+    c = a + b
+    ##
+    assert c.value == 17
+    assert c.unit == u.km
+    # Uncertainties under addition add in quadrature
+    assert c.uncertainty == math.sqrt(2**2 + 5**2)
+
+def test_subtraction_uquantity():
+    ###
+    a = UQuantity(5, u.km, 2)
+    b = UQuantity(12, u.km, 5)
+    c = b - a
+    ##
+    assert c.value == 7
+    assert c.unit == u.km
+    # Uncertainties under subtraction add in quadrature
+    assert c.uncertainty == math.sqrt(2**2 + 5**2)
