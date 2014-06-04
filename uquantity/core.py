17aebb10970b89090f70b5b65dda98ba0f63e981
Mabry Cervin <mpcervin@uncg.edu>
2014-06-04 13:23:22 -0400

    Initial code for inheriting Quantity

--- uquantity/core.py
+++ uquantity/core.py
@@ -1,9 +1,47 @@
 from astropy import units as u
-from uncertainties import ufloat
+import uncertainties
+import numpy as np
 
 
-class UQuantity(object):
+class UQuantity(u.Quantity):
 
+    def __new__(cls, value, uncertainty, unit=None, dtype=None, copy=True):
+
+        if isinstance(value, u.Quantity):
+            if unit is not None:
+                value = value.to(unit).value
+            else:
+                unit = value.unit
+                value = value.value
+
+        if isinstance(uncertainty, uncertainties.Variable):
+            uncertainty = uncertainty.std_dev
+
+
+        try:
+            value = np.asarray(value)
+        except ValueError as e:
+            raise TypeError(str(e))
+
+        if unit is None:
+            raise u.UnitsError("No unit was specified")
+
+        self = super(UQuantity, cls).__new__(
+                cls, value, uncertainty, unit, dtype=dtype, copy=copy)
+
+        return self
+
+    def __array_finalize__(self, obj):
+
+        if obj is None:
+            return
+
+        self.uncert_object = ufloat(getattr(obj, 'value', None),
+                getattr(obj, 'uncertainty', None))
+
+        self.quantity = getattr(obj, 'value', None) * getattr(obj, 'unit', None)
+
+    # To be replaced by __new__ and __array_finalize__
     def __init__(self, value, unit, uncertainty):
         self.value = value
         self.unit = unit
