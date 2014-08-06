a7ae7cfa71771483fa14f5f685c6927fc49c60dd
Mabry Cervin <mpcervin@uncg.edu>
2014-08-06 12:38:34 -0400

    Added comments for metaclass

--- uquantity/core.py
+++ uquantity/core.py
@@ -4,11 +4,13 @@ import uncertainties as uncert
 #import uncertlib.uncertainties as uncert
 
 
+# Eliminates base layout conflict by removing __slots__ fields from parent classes
 class SlotlessMeta(type):
     def __new__(meta, name, bases, dct):
 
         new_bases = ()
 
+        # Iterates through each parent class
         for base in bases:
             #print "Dict of %s: %s" % (base, base.__dict__)
             new_base_dict = base.__dict__.copy()
@@ -16,14 +18,13 @@ class SlotlessMeta(type):
                 del new_base_dict['__slots__']
                 new_base = type(str(base), (), new_base_dict)
             except KeyError:
-                print "No __slots__ in %s" % base
+                # Avoids touching parents that do not need to be modified
                 new_base = base
 
 
             new_bases = new_bases + (new_base,)
 
-        new = type(name, new_bases, dct)
-        return new
+        return type(name, new_bases, dct)
 
 
 class UQuantity(uncert.Variable, u.Quantity):
@@ -37,12 +38,17 @@ class UQuantity(uncert.Variable, u.Quantity):
             value = value.value
 
         value = float(value) # Makes uncertainties happy
-        self = super(UQuantity, cls).__new__(
-                cls, value, unit, dtype=dtype, copy=copy)
+        #self = super(UQuantity, cls).__new__(
+        #        cls, value, unit, dtype=dtype, copy=copy)
 
-        self._std_dev = uncertainty
+        self = u.Quantity.__new__(cls, value, unit)
+        self.isscalar = True
+
+        super(UQuantity, cls).__init__(self, value, uncertainty)
+        #self._std_dev = uncertainty
 
         return self
+
 
 
 
@@ -51,8 +57,10 @@ class UQuantity(uncert.Variable, u.Quantity):
         if obj is None:
             return
 
-        self._std_dev = getattr(obj, 'std_dev', None)
+        #self._std_dev = getattr(obj, 'std_dev', None)
 
+    def __float__(self):
+        return float(self.value)
 
     def __numpy_ufunc__(self, ufunc, method, i, inputs, **kwargs):
         print 'In UQuantity.__numpy_ufunc__'
